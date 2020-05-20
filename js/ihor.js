00dd3a19d12951f5d8e212cae6e3a33b52c0b880
Ihor Stefaniv <dezir16@gmail.com>
2020-05-20 21:34:19 +0300

    add second function

--- js/ihor.js
+++ js/ihor.js
@@ -6,3 +6,14 @@ function addTwoNumbers(x, y) {
 document.write('<h1>');
 document.write( 'Result: 5 + 2 = ' + addTwoNumbers(5, 2));
 document.write('</h1>');
+
+
+function addEventListener(firstWord, secondWord) {
+  if (firstWord === "write") {
+    secondWord();
+  }
+}
+
+addEventListener('write', function () {
+  document.write('Hello World!');
+});
