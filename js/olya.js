f9414b87cdf7074d6ed8a019d797835e5069d2e2
Olya Savliuk <olya.dytkevuch@gmail.com>
2020-05-22 14:15:48 +0300

    Added FizzBuzz program

--- js/olya.js
+++ js/olya.js
@@ -1,3 +1,4 @@
+/*// Loops
 document.write(`<h1>Amount of animals is ${animals.length}</h1>`);
 
 for (let i = 0; i < animals.length; i++) {
@@ -8,7 +9,7 @@ for (let i = 0; i < animals.length; i++) {
   document.write('Age ' + animal.age + '<br>');
 }
 
-//Addition function
+// Addition function
 function addTwoNumbers(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
@@ -25,10 +26,30 @@ addEventListenerNew(function () {
   document.write(`<h2>Hello world!</h2>`);
 });
 
+//Function with SetTimeout
 for (var i = 1; i < 21; i++) {
   (function(j) {
     setTimeout(function () {
       document.write(j + '<br>');
     }, 1000);
   })(i);
+}*/
+
+// FizzBuzz
+document.write('<h2>FizzBuzz Program</h2>');
+
+function FizzBuzz(firstNumber,lastNumber) {
+ for (let firstNumber = 1; firstNumber < (lastNumber+1); firstNumber++) {
+  if (firstNumber % 3 === 0 && firstNumber % 5 === 0) {
+    document.write("FizzBuzz"+'<br>');
+  } else if (firstNumber % 3 === 0) {
+    document.write("Fizz"+'<br>');
+  } else  if (firstNumber % 5 === 0) {
+    document.write("Buzz"+'<br>');
+  } else {
+    document.write(firstNumber+'<br>');
+  }
+ }
 }
+ 
+FizzBuzz(1, 100);
